640296720d357fc02c9d0750941ac0b0ea40f5ff
Ben Eggers <ben@beneggers.com>
2024-04-19 21:44:35 -0700

    basic parsing for basic boys and girls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use pest_derive::Parser;
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct TLAParser;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("testdata/string_test_module.tla").expect("Something went wrong reading the file");
+    let parsed = TLAParser::parse(Rule::source_file, &contents).unwrap();
+    println!("{:?}", parsed);
 }
