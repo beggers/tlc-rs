3d2657c6108fd6d531b186ff1abe38881d9ba182
Ben Eggers <ben@beneggers.com>
2024-04-20 12:33:50 -0700

    mv dumb little module out of testdata

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod parser;
 use crate::parser::parse_file;
 
 fn main() {
-  let filename = "testdata/string_test_module.tla";
+  let filename = "string_test_module.tla";
   let _ = parse_file(filename);
 }
